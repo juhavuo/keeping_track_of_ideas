ef22d6e9e321398f909e3f6c6a5a9a732bba7f0e
juhavuo <juha.vuokko@pp.inet.fi>
2019-04-09 13:45:18 +0300

    get methods: all, public, public at certain timeperiod

--- routes/idearoute.js
+++ routes/idearoute.js
@@ -6,9 +6,6 @@ const mongoose = require('mongoose');
 
 router.post('/', (req, res) => {
 
-
-
-  // first with fixed values
   /*
   const owner = req.body.owner;
   const private_var = req.body.private;
@@ -34,7 +31,41 @@ router.post('/', (req, res) => {
       error: err
     })
   });
+});
 
+//for testing purposes, maybe for superuser, needs to add authorization
+router.get('/all', (req,res)=>{
+  Idea.find()
+  .exec()
+  .then(docs => {
+    res.status(200).json(docs);
+  }).catch(err =>{
+    res.status(500).json({error,err});
+  });
+});
+
+//get all public ideas, this is for all
+router.get('/public', (req,res) =>{
+  Idea.find({'private': false})
+  .exec()
+  .then(docs => {
+    res.status(200).json(docs);
+  }).catch(err =>{
+    res.status(500).json({error,err});
+  });
+});
+
+//testing to get messages from certain time period
+router.get('/public/timetest', (req,res) =>{
+  const timeline_begin = req.body.timeline_begin;
+  const timeline_end = req.body.timeline_end;
+  Idea.find({$and: [{'private': false},{'time':{$gt: new Date(timeline_begin), $lt: new Date(timeline_end)}}]})
+  .exec()
+  .then(docs =>{
+    res.status(200).json(docs);
+  }).catch(err=>{
+    res.status(500).json(docs);
+  });
 });
 
 module.exports = router;
